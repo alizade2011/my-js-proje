f904a08b91fbdddfe500b455dde13288f2f548fd
Tamerlan Alizade <tamerlanalizade2011@gmail.com>
2024-04-14 09:16:54 +0300

    test

---
+++ java.js
@@ -0,0 +1,34 @@
+let num1 = 5;
+let  num2= 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+
+let a = 10; 
+let b = 20; 
+  
+// Subtract directlty  
+console.log(a - b); // -10 
+console.log(b - a); // 10  
+  
+// Subtract by using another variable 
+let c = b - a; 
+console.log(c); // 10 
+
+ // First variable 
+ let x = 40; 
+  
+ // Second variable 
+ let y = 20; 
+   
+ // Funciton for dividing the variable 
+ function divide(x, y) { 
+   return x / y; 
+ } 
+   
+ // For printing the result 
+ console.log("After division : " + divide(x, y)); 
